786965b79a375f62eaefb44509fc5f694c5952d2
Mark Gregg <gregg.mark@gmail.com>
2025-12-30 19:59:37 +0000

    fix bundle size check

--- .size-limit.js
+++ .size-limit.js
@@ -1,32 +1,32 @@
 module.exports = [
   {
     name: '@smart-input/core',
-    path: 'packages/core/dist/core.min.mjs',
+    path: 'packages/core/dist/core.min.js',
     limit: '50 KB',
   },
   {
     name: '@smart-input/typeahead',
-    path: 'packages/typeahead/dist/typeahead.min.mjs',
+    path: 'packages/typeahead/dist/typeahead.min.js',
     limit: '30 KB',
   },
   {
     name: '@smart-input/reactblocks',
-    path: 'packages/reactblocks/dist/reactblocks.min.mjs',
+    path: 'packages/reactblocks/dist/reactblocks.min.js',
     limit: '20 KB',
   },
   {
     name: '@smart-input/dragblocks',
-    path: 'packages/dragblocks/dist/dragblocks.min.mjs',
+    path: 'packages/dragblocks/dist/dragblocks.min.js',
     limit: '20 KB',
   },
   {
     name: '@smart-input/dropcontent',
-    path: 'packages/dropcontent/dist/dropcontent.min.mjs',
+    path: 'packages/dropcontent/dist/dropcontent.min.js',
     limit: '15 KB',
   },
   {
     name: '@smart-input/commitnotifier',
-    path: 'packages/commitnotifier/dist/commitnotifier.min.mjs',
+    path: 'packages/commitnotifier/dist/commitnotifier.min.js',
     limit: '15 KB',
   },
 ];
